db07e68ecfd26d1af9f745ad2287f835129b9be6
musarratChowdhury <muhitlopez143@gmail.com>
2021-09-08 12:31:53 +0600

    updated

--- migratoryBirds.js
+++ migratoryBirds.js
@@ -1,53 +1,58 @@
-let testarr = [2, 4, 3, 2, 3, 1, 2, 1, 3, 3];
+let testarr = [2, 4, 3, 2, 3, 1, 2, 1, 3, 3, 2];
 
 function migratoryBirds(arr) {
-  // Write your code here
   let result;
 
-  let birdType = [
-    { type: 1, count: 0 },
-    { type: 2, count: 0 },
+  let counterArr = [
+    { type: 1, count: 0 }, //index = 0;
+    { type: 2, count: 0 }, //index = 1;
     { type: 3, count: 0 },
-    { type: 4, count: 0 },
-    { type: 5, count: 0 },
+    { type: 4, count: 0 }, //index = 3;
+    { type: 5, count: 0 }, //index = 4;
   ];
-  for (let i = 0; i < arr.length; i++) {
-    let index = arr[i] - 1;
 
-    switch (index) {
-      case 0:
-        birdType[index].count++;
-        break;
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
       case 1:
-        birdType[index].count++;
+        counterArr[arr[i] - 1].count++;
         break;
       case 2:
-        birdType[index].count++;
+        counterArr[arr[i] - 1].count++;
         break;
       case 3:
-        birdType[index].count++;
+        counterArr[arr[i] - 1].count++;
         break;
       case 4:
-        birdType[index].count++;
+        counterArr[arr[i] - 1].count++;
+        break;
+      case 5:
+        counterArr[arr[i] - 1].count++;
         break;
     }
-    console.log(index);
-    console.log(birdType[index]);
   }
-  console.log(birdType);
-
-  let sortedArr = birdType.sort((a, b) => a.count - b.count).reverse();
-  //console.log(sortedArr);
-  let maxCount = sortedArr[0].count;
-  let onlyMaxCountedBirds = sortedArr.filter((x) => {
-    if (x.count == maxCount) return x;
+  console.log(counterArr);
+  //lets sort the array form small to large
+  let sortedArr = counterArr.sort((a, b) => {
+    return a.count - b.count;
   });
-  //console.log(onlyMaxCountedBirds);
-  if (onlyMaxCountedBirds.length == 1) {
-    result = sortedArr[0].type;
+  //finding the maximum number of sightings
+  console.log(sortedArr);
+  let maxCount = sortedArr[sortedArr.length - 1].count;
+  //filetered out the types with maxinum counts
+  let maxcountedArr = sortedArr.filter((x) => {
+    if (x.count == maxCount) {
+      return x;
+    }
+  });
+  console.log(maxcountedArr);
+
+  if (maxcountedArr.length == 1) {
+    result = maxcountedArr[0].type;
   } else {
-    let lowerType = onlyMaxCountedBirds.sort((a, b) => a.type - b.type);
-    console.log(lowerType[0]);
+    let lowerType = maxcountedArr.sort((a, b) => {
+      return a.type - b.type;
+    });
+    // console.log(lowerType);
     result = lowerType[0].type;
   }
 
